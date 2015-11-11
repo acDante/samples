7d28cc0bcaaeda079c97621a17f6eda1f618d87c
Tetsunari Inamura <inamura@nii.ac.jp>
2015-11-12 06:12:38 +0900

    Modify to adjust for 2.3.2; Indent cosme

--- ROS_integration/sig_ros/src/sig_controller.cpp
+++ ROS_integration/sig_ros/src/sig_controller.cpp
@@ -1,38 +1,32 @@
-#include "Controller.h"
-#include "Logger.h"
-#include "ControllerEvent.h"
-#include "ros/ros.h"
 #include <sstream>
 #include "std_msgs/String.h"
 #include <sig_ros/Velocity.h>
 
-
-#define PI 3.141592
-#define DEG2RAD(DEG) ( (PI) * (DEG) / 180.0 )
-#define ARY_SIZE(ARY) ( (int)(sizeof(ARY)/sizeof(ARY[0])) )
-
+#include "ros/ros.h"
+#include "sigverse/commonlib/Controller.h"
+#include "sigverse/commonlib/Logger.h"
+#include "sigverse/commonlib/ControllerEvent.h"
 
 
 class SendController : public Controller
 {
 public:
-  //シミュレーション開始時に一度だけ呼出される関数onInitの利用を宣言します
-  void onInit(InitEvent &evt);
-  double onAction(ActionEvent &evt);
-  void onRecvMsg(RecvMsgEvent &evt);
-  void poseCallback(const sig_ros::VelocityConstPtr& vel);
+	void   onInit(InitEvent &evt);
+	double onAction(ActionEvent &evt);
+	void   onRecvMsg(RecvMsgEvent &evt);
+	void   poseCallback(const sig_ros::VelocityConstPtr& vel);
 
 public:
-RobotObj *my;
+	RobotObj *my;
 
-  double velocity;
-  int i;
-  ros::Publisher chatter_pub;
-  ros::Subscriber  pose_sub ;
- int j;
+	double velocity;
+	int i;
+	ros::Publisher chatter_pub;
+	ros::Subscriber  pose_sub ;
+	int j;
 
- double Weel_one;
- double Weel_two;
+	double Weel_one;
+	double Weel_two;
 };
 
 
@@ -40,61 +34,59 @@ void SendController::poseCallback(const sig_ros::VelocityConstPtr& vel)
 {
     Weel_one =  vel->linear;
     Weel_two =  vel->angular;
-
 }
+
 
 void SendController::onInit(InitEvent &evt)
 {
+	Weel_one = 0.0;
+	Weel_two = 0.0;
 
- Weel_one = 0.0;
- Weel_two = 0.0;
+	int argc =0;
+	char** argv = NULL;
+	my = this->getRobotObj(this->myname());
+	my->setWheel(10.0, 10.0);
+	ros::init(argc, argv, "sig_controller_node");
+	ros::NodeHandle n;
+	velocity = 1;
 
-int argc =0;
-char** argv = NULL;
-my = this->getRobotObj(this->myname());
-my->setWheel(10.0, 10.0);
-ros::init(argc, argv, "sig_controller_node");
-ros::NodeHandle n;
-velocity = 1;
+	chatter_pub = n.advertise<std_msgs::String>("message_command", 1);
 
-chatter_pub = n.advertise<std_msgs::String>("message_command", 1);
+	pose_sub = n.subscribe<sig_ros::Velocity>("mycommand_velocity", 1,&SendController::poseCallback,this);
+	ros::Rate loop_rate(10);
 
-pose_sub = n.subscribe<sig_ros::Velocity>("mycommand_velocity", 1,&SendController::poseCallback,this);
-ros::Rate loop_rate(10);
-
-// Running the ros_command node
- system ("source ~/catkin_ws/devel/setup.sh");
- system ("rosrun sig_ros ros_controller &");
+	// Running the ros_command node
+	system ("source ~/catkin_ws/devel/setup.sh");
+	system ("rosrun sig_ros ros_controller &");
 
 }
+
 
 double SendController::onAction(ActionEvent &evt)
 {
-my->setWheelVelocity(Weel_one,Weel_two);
- ros::spinOnce();
-  //1秒おきにonActionが呼び出されます
-  return 0.01;
-}
+	my->setWheelVelocity(Weel_one,Weel_two);
+	ros::spinOnce();
 
+	return 0.01;
+}
 
 
 void SendController::onRecvMsg(RecvMsgEvent &evt)
 {
- std::string sender = evt.getSender();
+	std::string sender = evt.getSender();
 
-  // 文字列を取得
-  std::string msg = evt.getMsg();
-  LOG_MSG(("message : %s", msg.c_str()));
-  std_msgs::String mesg;
-  std::stringstream ss;
-  //メッセージが"Hello!!"なら手を上げます
-  ss << msg;
-  mesg.data = ss.str();
-  chatter_pub.publish(mesg);
+	std::string msg = evt.getMsg();
+	LOG_MSG(("message : %s", msg.c_str()));
+	std_msgs::String mesg;
+	std::stringstream ss;
+
+	ss << msg;
+	mesg.data = ss.str();
+	chatter_pub.publish(mesg);
 }
 
 
 extern "C"  Controller * createController ()
 {
-  return new SendController;
+	return new SendController;
 }
